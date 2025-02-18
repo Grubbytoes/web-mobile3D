dcb234630a002379764d710da6cea853bcc12738
hughie <=>
2025-02-18 16:25:29 +0000

    * changed indentation style * added can

--- scriptCola.js
+++ scriptCola.js
@@ -1,49 +1,81 @@
-var scene, camera, renderer, box;
+const assetPath = "assets/models/";
+
+var scene, camera, renderer, clock, box, mixer, actions = [], mode;
 
 initialise()
 
 function initialise() {
-    console.log("Hello, World!");
-    
+  console.log("Hello, World!");
 
-    // Getting camera, scene, and renderer
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x011638);
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 3;
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+  // Clock
+  clock = new THREE.Clock();
 
-    // Lights
-    const lightMain = new THREE.PointLight( 0xffffff, 1, 100 );
-    lightMain.position.set(0, 0, 10);
-    scene.add(lightMain);
+  // Getting camera, scene, and renderer
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x011638);
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 15;
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-    // Material and mesh
-    const material = new THREE.MeshStandardMaterial();
-    material.color.set(0xcdcdcd)
-    const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+  // Lights
+  const lightPoint = new THREE.PointLight(0x404040, 4, 200);
+  const lightAmbient = new THREE.AmbientLight(0x011638, 1);
+  lightPoint.position.set(4, 8, 16);
+  scene.add(lightPoint, lightAmbient);
 
-    // Adding the box
-    box = new THREE.Mesh(cubeGeometry, material);
-    box.position.set(0, 0, 0);
-    scene.add(box);
+  // Controls
+  const controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 0, 0);
+  mode = "open";
+  const btn = document.getElementById("btn");
+  btn.addEventListener('click', () => {
+    console.log("You clicked meh.")
+    if (mode == "open") {
+      actions.array.forEach(act => {
+        // This assumes we have some animations in actions
+        act.timeScale = 1;
+        act.reset();
+        act.play();
+      });
+    }
+  })
 
-    // Resize logic
-    window.addEventListener('resize', onresize, false);
-    update();
+  // Resize logic
+  window.addEventListener('resize', onresize, false);
+
+  animate();
 }
 
-function update() {
-    requestAnimationFrame(update);
-    renderer.render(scene, camera);
-    box.rotation.y += 0.02;
-    box.rotation.z += 0.01;
+// loader
+const loader = new THREE.GLTFLoader();
+loader.load(assetPath + "soda_can_opening.glb", (gltf) => {
+
+  const model = gltf.scene;
+  scene.add(model);
+  scene.position.y = 6;
+
+  mixer = new THREE.AnimationMixer(model);
+  const animations = gltf.animations;
+  animations.forEach(clip => {
+    const action = mixer.clipAction(clip)
+    actions.push(action)
+  });
+})
+
+function animate() {
+  requestAnimationFrame(animate);
+  
+  if (mixer) {
+    mixer.update(clock.getDelta());
+  }
+
+  renderer.render(scene, camera);
 }
 
 function onresize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
